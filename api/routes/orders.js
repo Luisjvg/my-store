91e0979ab9c40d5f2120caf352b2562787924559
Luis <luisjov712@gmail.com>
2023-03-31 22:57:58 -0300

    p

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,71 +1,71 @@
-const express = require('express');
-const OrderService = require('../service/order')
-const validator = require('../middleware/validator')
-const { getOrderSchema, createOrderSchema, addItemSchema } = require('../schemas/orderSchema');
+// const express = require('express');
+// const OrderService = require('../service/order')
+// const validator = require('../middleware/validator')
+// const { getOrderSchema, createOrderSchema, addItemSchema } = require('../schemas/orderSchema');
 
-const router = express.Router();
-const service = new OrderService();
+// const router = express.Router();
+// const service = new OrderService();
 
-router.get('/',
-async (req, res, next) =>{
-  try{
-    const orders = await service.find();
-    res.send(orders);
-  }catch(e){
-    next(e)
-  }
-})
+// router.get('/',
+// async (req, res, next) =>{
+//   try{
+//     const orders = await service.find();
+//     res.send(orders);
+//   }catch(e){
+//     next(e)
+//   }
+// })
 
-router.get('/filter', (req, res) =>{
+// router.get('/filter', (req, res) =>{
 
-})
+// })
 
-router.get('/:id',
-validator(getOrderSchema, 'params'),
-async (req, res, next) =>{
-  try{
-    const { id } = req.params;
-    const order = await service.findOne(id)
-    res.json(order) ;
-  }catch(e){
-    next(e)
-  }
-})
+// router.get('/:id',
+// validator(getOrderSchema, 'params'),
+// async (req, res, next) =>{
+//   try{
+//     const { id } = req.params;
+//     const order = await service.findOne(id)
+//     res.json(order) ;
+//   }catch(e){
+//     next(e)
+//   }
+// })
 
-router.post('/',
-validator(createOrderSchema, 'body'),
-async (req, res, next) =>{
-  try{
-    const body = req.body;
-    const newOrder = await service.create(body);
-    res.status(201).json(newOrder)
-  }catch(e){
-    next(e)
-  }
-})
+// router.post('/',
+// validator(createOrderSchema, 'body'),
+// async (req, res, next) =>{
+//   try{
+//     const body = req.body;
+//     const newOrder = await service.create(body);
+//     res.status(201).json(newOrder)
+//   }catch(e){
+//     next(e)
+//   }
+// })
 
-router.post('/add-item',
-validator(addItemSchema, 'body'),
-async (req, res, next) =>{
-  try{
-    const body = req.body;
-    const newItem = await service.addItem(body);
-    res.status(201).json(newItem)
-  }catch(e){
-    next(e)
-  }
-})
-
-
-router.patch('/:id',
-async (req, res, next) =>{
-
-})
-router.delete('/:id', async (req, res) =>{
-
-})
+// router.post('/add-item',
+// validator(addItemSchema, 'body'),
+// async (req, res, next) =>{
+//   try{
+//     const body = req.body;
+//     const newItem = await service.addItem(body);
+//     res.status(201).json(newItem)
+//   }catch(e){
+//     next(e)
+//   }
+// })
 
 
+// router.patch('/:id',
+// async (req, res, next) =>{
 
-module.exports = router;
+// })
+// router.delete('/:id', async (req, res) =>{
+
+// })
+
+
+
+// module.exports = router;
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,56 +1,56 @@
-const UserService = require('../service/user')
-const validator = require('../middleware/validator')
-const {updateUserSchema, createUserSchema, getUserSchema} = require('../schemas/schemas')
+// const UserService = require('../service/user')
+// const validator = require('../middleware/validator')
+// const {updateUserSchema, createUserSchema, getUserSchema} = require('../schemas/schemas')
 
-const express = require('express');
-const router = express.Router();
-const service = new UserService();
+// const express = require('express');
+// const router = express.Router();
+// const service = new UserService();
 
-// router.get('/', (req, res) =>{
-//   const { limit, offset } = req.query;
-//   if(limit && offset){
-//     res.json({
-//       limit,
-//       offset
-//     })
-//   }else{
-//       res.send('No hay parametros');
+// // router.get('/', (req, res) =>{
+// //   const { limit, offset } = req.query;
+// //   if(limit && offset){
+// //     res.json({
+// //       limit,
+// //       offset
+// //     })
+// //   }else{
+// //       res.send('No hay parametros');
+// //   }
+// // })
+
+// router.get('/', async (req, res, next) =>{
+//   try{
+//     const users = await service.find();
+//     res.send(users)
+//   }catch(e){
+//     next(e)
 //   }
 // })
 
-router.get('/', async (req, res, next) =>{
-  try{
-    const users = await service.find();
-    res.send(users)
-  }catch(e){
-    next(e)
-  }
-})
+// router.get('/:id', async (req, res, next) =>{
+//   try{
+//     const { id } = req.params;
+//     const user = await service.findOne(id);
+//     res.send(user);
+//   }catch(e){
+//     next(e)
+//   }
+// })
 
-router.get('/:id', async (req, res, next) =>{
-  try{
-    const { id } = req.params;
-    const user = await service.findOne(id);
-    res.send(user);
-  }catch(e){
-    next(e)
-  }
-})
+// router.post('/', async (req, res) =>{
+//   const body = req.body;
+//   const newUser = await service.create(body)
+//   res.send(newUser);
+// })
 
-router.post('/', async (req, res) =>{
-  const body = req.body;
-  const newUser = await service.create(body)
-  res.send(newUser);
-})
+// router.delete('/:id', async (req, res, next) =>{
+//   try{
+//     const { id } = req.params;
+//     const deleteUser = await service.delete(id);
+//     res.send(deleteUser) ;
+//   }catch(e){
+//     next(e)
+//   }
+// })
 
-router.delete('/:id', async (req, res, next) =>{
-  try{
-    const { id } = req.params;
-    const deleteUser = await service.delete(id);
-    res.send(deleteUser) ;
-  }catch(e){
-    next(e)
-  }
-})
-
-module.exports = router;
+// module.exports = router;
